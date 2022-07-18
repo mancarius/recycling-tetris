f719a4a9c364723b034e22efb6e162e84d30ce94
Mattia <75211269+mancarius@users.noreply.github.com>
2022-07-18 18:13:54 +0200

    added footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p><span>Developed by <a href="www.mattiamancarella.com">Mattia Mancarella</a> as a Vue.js case study for <a
+          href="https://start2impact.it">Start2impact</a>.</span></p>
+    <p><span>The source code is available on <a href="https://github.com/mancarius/recycling-tetris">Github</a></span>
+    </p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  margin-top: 2rem;
+  margin-bottom: 0;
+  padding-top: 2rem;
+  font-size: 80%;
+  max-width: 600px;
+
+  p {
+    display: inline-block;
+    line-height: 1.3em;
+
+    span {
+      background-color: azure;
+      padding: 3px;
+    }
+  }
+}
+</style>
