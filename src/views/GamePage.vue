239ab8e9d5aef78a7071c315a71cc248d203b918
Mattia <75211269+mancarius@users.noreply.github.com>
2022-07-19 14:12:47 +0200

    updated import path

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -3,8 +3,8 @@ import State from "@/@types/state.interface";
 import Getters from "@/utils/enums/Getters";
 import { computed } from "@vue/runtime-core";
 import { useStore } from "vuex";
-import GameBoard from "@/components/GameBoard.vue";
-import GameOptions from "@/components/GameOptions.vue";
+import GameBoard from "@/components/game/GameBoard.vue";
+import GameOptions from "@/components/game/GameOptions.vue";
 
 const store = useStore<State>();
 const showOptions = computed(() => !store.getters[Getters.GAME_IS_RUNNING]);
