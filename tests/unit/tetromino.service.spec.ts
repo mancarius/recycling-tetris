250327f6d78491af277f8f1bfb5087f2ab934041
mancarius <mattia.mancarella.dev@gmail.com>
2023-04-16 12:51:15 +0200

    tetromino.service.spec - test refactoring

--- tests/unit/tetromino.service.spec.ts
+++ tests/unit/tetromino.service.spec.ts
@@ -5,72 +5,79 @@ import { mockTetromino, shape } from "@/mocks/tetromino";
 import {
   getTetrominosFinalVerticalProjection,
   getSpawnPosition,
-  rotateShapePoint,
-  getShapeCoordsByPosition,
   countFreeRowsLength,
 } from "../../src/services/tetromino.service";
 
 describe("TetrominoService", () => {
-  beforeEach(() => {/* */});
+  beforeEach(() => {/* */ });
 
-  it("getSpawnPosition with rotation 90", () => {
-    const initialPosition = { x: 4, y: 10 };
-    const tetromino: TetrominoState = {
-      ...mockTetromino,
-      position: initialPosition,
-      shape: shape.I,
-      rotation: 90,
-    };
-    const expectedPosition = { x: 4, y: -1 };
-    const finalProjection = getSpawnPosition(tetromino);
+  describe('getSpawnPosition', () => {
 
-    expect(finalProjection).toStrictEqual(expectedPosition);
+    it("getSpawnPosition with rotation 90", () => {
+      const initialPosition = { x: 4, y: 10 };
+      const tetromino: TetrominoState = {
+        ...mockTetromino,
+        position: initialPosition,
+        shape: shape.I,
+        rotation: 90,
+      };
+      const expectedPosition = { x: 4, y: -1 };
+      const finalProjection = getSpawnPosition(tetromino);
+
+      expect(finalProjection).toStrictEqual(expectedPosition);
+    });
+
+    it("getSpawnPosition with rotation 0", () => {
+      const initialPosition = { x: 4, y: 0 };
+      const tetromino: TetrominoState = {
+        ...mockTetromino,
+        position: initialPosition,
+        shape: shape.I,
+        rotation: 0,
+      };
+      const expectedPosition = { x: 4, y: -3 };
+      const finalProjection = getSpawnPosition(tetromino);
+
+      expect(finalProjection).toStrictEqual(expectedPosition);
+    });
   });
 
-  it("getSpawnPosition with rotation 0", () => {
-    const initialPosition = { x: 4, y: 0 };
-    const tetromino: TetrominoState = {
-      ...mockTetromino,
-      position: initialPosition,
-      shape: shape.I,
-      rotation: 0,
-    };
-    const expectedPosition = { x: 4, y: -3 };
-    const finalProjection = getSpawnPosition(tetromino);
+  describe('countFreeRowsLength', () => {
+    
+    it("countFreeRowsLength should return 9", () => {
+      const initialPosition = { x: 4, y: 5 };
+      const expectedResult = 9;
+      const result = countFreeRowsLength(initialPosition, mockGrid);
 
-    expect(finalProjection).toStrictEqual(expectedPosition);
+      expect(result).toEqual(expectedResult);
+    });
+
+    it("countFreeRowsLength should return 0", () => {
+      const initialPosition = { x: 4, y: 14 };
+      const expectedResult = 0;
+      const result = countFreeRowsLength(initialPosition, { ...mockGrid });
+
+      expect(result).toEqual(expectedResult);
+    });
   });
 
-  it("countFreeRowsLength should return 9", () => {
-    const initialPosition = { x: 4, y: 5 };
-    const expectedResult = 9;
-    const result = countFreeRowsLength(initialPosition, mockGrid);
+  describe('getTetrominosFinalVerticalProjection', () => {
 
-    expect(result).toEqual(expectedResult);
-  });
+    it("getTetrominosFinalVerticalProjection should return y=14", () => {
+      const initialPosition = { x: 4, y: 5 };
+      const tetromino: TetrominoState = { ...mockTetromino, position: initialPosition, rotation: 90 };
+      const expectedPosition = { x: 4, y: 14 };
+      const finalPosition = getTetrominosFinalVerticalProjection(tetromino, { ...mockGrid });
 
-  it("countFreeRowsLength should return 0", () => {
-    const initialPosition = { x: 4, y: 14 };
-    const expectedResult = 0;
-    const result = countFreeRowsLength(initialPosition, { ...mockGrid });
+      expect(finalPosition).toEqual(expectedPosition);
+    });
 
-    expect(result).toEqual(expectedResult);
-  });
+    it("getTetrominosFinalVerticalProjection should return the initial y", () => {
+      const initialPosition = { x: 4, y: 14 };
+      const tetromino: TetrominoState = { ...mockTetromino, position: initialPosition, rotation: 90 };
+      const finalPosition = getTetrominosFinalVerticalProjection(tetromino, { ...mockGrid });
 
-  it("getTetrominosFinalVerticalProjection should return y=14", () => {
-    const initialPosition = { x: 4, y: 5 };
-    const tetromino: TetrominoState = { ...mockTetromino, position: initialPosition, rotation: 90 };
-    const expectedPosition = { x: 4, y: 14 };
-    const finalPosition = getTetrominosFinalVerticalProjection(tetromino, { ...mockGrid });
-
-    expect(finalPosition).toEqual(expectedPosition);
-  });
-
-  it("getTetrominosFinalVerticalProjection should return the initial y", () => {
-    const initialPosition = { x: 4, y: 14 };
-    const tetromino: TetrominoState = { ...mockTetromino, position: initialPosition, rotation: 90 };
-    const finalPosition = getTetrominosFinalVerticalProjection(tetromino, { ...mockGrid });
-
-    expect(finalPosition).toEqual(initialPosition);
+      expect(finalPosition).toEqual(initialPosition);
+    });
   });
 });
